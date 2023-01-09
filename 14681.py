a8c848baf4d427dd71e94458dd052705b65dd6fe
SangJun <73101224+goodpinokio@users.noreply.github.com>
2023-01-09 18:29:02 +0900

    14681 백준문제
    백준 코딩
    입력: 첫 줄에는 정수 x가 주어진다. (−1000 ≤ x ≤ 1000; x ≠ 0) 다음 줄에는 정수 y가 주어진다. (−1000 ≤ y ≤ 1000; y ≠ 0)
    
    출력: 점 (x, y)의 사분면 번호(1, 2, 3, 4 중 하나)를 출력한다.

---
+++ 14681.py
@@ -0,0 +1,15 @@
+x = int(input())
+y = int(input())
+
+if x >0:
+    if y>0:
+        print('1')
+if x>0:
+    if y<0:
+        print('4')
+if x<0:
+    if y>0:
+        print('2')
+if x <0:
+    if y<0:
+        print('3')
